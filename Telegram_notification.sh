f6af59da7796713e513ce3996ea8ed4b816c832b
Gani Rakhmatov <g_rahmatov@mail.ru>
2018-01-01 18:31:15 +0500

    Update Telegram_notification.sh

--- Telegram_notification.sh
+++ Telegram_notification.sh
@@ -9,7 +9,7 @@ ip="$(hostname -i)"
 if [ -n "$SSH_CLIENT" ] ; then
         ip_address=$(echo $SSH_CLIENT | awk '{print $1}')
 
-        message="New remote SSH connection : "$USER"@"$hostname" serverIP="$ip" from "$ip_address" "$date
+        message="New remote SSH connection : "$USER"@"$(hostname)" serverIP="$ip" from "$ip_address" "$date
 
         curl -s -d "chat_id="$telegramchatid"&text=$message&parse_mode=markdown&disable_web_page_preview=1" $url > /dev/null 2>&1
 fi
