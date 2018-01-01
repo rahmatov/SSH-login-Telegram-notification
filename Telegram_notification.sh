aea114780e56b84c70e8688c252c82296ffdfaca
Gani Rakhmatov <g_rahmatov@mail.ru>
2018-01-01 18:15:26 +0500

    Update Telegram_notification.sh

--- Telegram_notification.sh
+++ Telegram_notification.sh
@@ -5,22 +5,12 @@ telegramchatid="79796"
 url="https://api.telegram.org/bot"$telegrambot"/sendMessage"
 
 date="$(date "+%F %T")"
-
 ip="$(hostname -i)"
-
 if [ -n "$SSH_CLIENT" ] ; then
         ip_address=$(echo $SSH_CLIENT | awk '{print $1}')
 
-        reverse_dns=`dig -x $ip_address +short`
-
-        if [ -z "$reverse_dns" ] ; then
-                reverse_dns=$ip_address
-        fi
-
-        message="New remote SSH connection : "$USER"@"$ip" from ["$reverse_dns"](http://whatismyipaddress.com/ip/"$ip_address"#General-IP-Information)
-"$date
+        message="New remote SSH connection : "$USER"@"$ip" from "$ip_address" "$date
 
         curl -s -d "chat_id="$telegramchatid"&text=$message&parse_mode=markdown&disable_web_page_preview=1" $url > /dev/null 2>&1
 fi
-
 
